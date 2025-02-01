c4c566d1fd6834660b201c928b6db854d6c3e28e
Marcelo Motta <128328762+mottamarcelo@users.noreply.github.com>
2025-02-01 09:14:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ballot = new Dictionary<string, int>();
+
+            Console.WriteLine("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+
+                        if (ballot.ContainsKey(name))
+                        {
+                            ballot[name] += votes;
+                        }
+                        else
+                        {
+                            ballot[name] = votes;
+                        }
+
+                    }
+
+                    foreach (var item in ballot)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
